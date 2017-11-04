2ca206ffb49f0191d36ea053509c921c5e0268e9
adam.kammoun <123456adam>
2017-11-04 20:08:33 +0100

    Initial commit

---
+++ src/main/java/fr/soat/soadle/SoadleApplication.java
@@ -0,0 +1,12 @@
+package fr.soat.soadle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoadleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoadleApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/soat/soadle/SoadleApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.soat.soadle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoadleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
