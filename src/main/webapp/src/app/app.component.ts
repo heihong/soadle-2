a96c3175aca67127577d35d16899d2bb89ec7e04
heihong <traingheihong@gmail.com>
2017-11-26 13:02:51 +0100

    google-event

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component , OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
   result = {};
+  user ={};
+  resulUser = {};
+  
+  ngOnInit() {
+      this.getUser()
+      .subscribe(response => this.resulUser = response);
+  }
+
 
   constructor(private http: Http){
   }
 
   private sayHello(): void {
-    this.http.get(`/api/v1/doodle/2bawsrkfwanfrhqp`).subscribe(response => this.result = response);
+    this.http.get(`/api/v1/doodle/s3uqv8qkvx3gvkz7`)
+    .subscribe(response => this.result = response.json());
   }
+
+  private getUser() {
+    return   this.http.get(`/user/v1/user`)
+      .map((response : Response) => this.result = response.json())
+      .catch(this.handleError);
+
+    }
+
+    handleError(error : Response){
+      console.log(error);
+      return Observable.throw(error);
+    }
+
+     private logout(): void {
+      this.http.get(`/logout`)
+      .subscribe(response => this.result = null);
+    }
 
 }
