09ff3a5a82d44656ac275ecc4504ff1a1f80ffaf
benchabana <abdelhakim.benchabana@soat.fr>
2017-12-01 23:51:32 +0100

    Ajout participation soadle

---
+++ src/main/java/fr/soat/soadle/repositories/ParticipantRepository.java
@@ -0,0 +1,17 @@
+package fr.soat.soadle.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import fr.soat.soadle.model.Participant;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+	
+	
+	@Query("select p from Participant p where MEETING_ID = :idMeeting and p.email = :email ")	
+	List<Participant>  findByEmail(@Param("idMeeting") String idMeeting, @Param("email") String email);
+
+}
